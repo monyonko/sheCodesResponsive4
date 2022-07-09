95ec0d1249fcbd6541b159d04cf7d2d6f2437454
monyonko <monyonko.miriam@gmail.com>
2022-07-09 15:49:54 +0300

    weekFour

---
+++ index.js
@@ -0,0 +1,19 @@
+hamburger = document.querySelector("#hamburger");
+nestedList = document.querySelector('.nested-list')
+
+nestedList.style.display = "none";
+hamburger.addEventListener('click', ()=>{
+    if (nestedList.style.display === "none"){
+        nestedList.style.display = "block"
+        hamburger.style.transform = "rotate(160deg)"
+       
+    }
+    else{
+        nestedList.style.display = "none";
+        hamburger.style.transform = "rotate(0deg)"
+        console.log(nestedList)
+
+    }
+  
+   
+})
